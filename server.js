4bd7c324a24b8bc165995fd912d2b8c63482b98b
nicoleweese <nweese@ymail.com>
2018-01-26 12:11:49 -0800

    test

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+const cors = require('cors');
+const fs = require('fs');
+const express = require('express');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
+client.connect();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/test', (req, res) => res.send('hello world'));
+
+app.listen(PORT, () => {
+    console.log(`currently listening on ${PORT}`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: '../client-rainy-day' })
+})
